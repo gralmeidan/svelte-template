01e0606cf53bb4acbfd3596a5ff09d0526752885
gralmeidan <gralmeidan@gmail.com>
2023-03-26 13:15:38 -0400

    chore: improved eslint support for typescript and svelte files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,28 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
-  plugins: ['svelte3'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['svelte3', '@typescript-eslint'],
   overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  rules: {
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    'svelte3/ignore-styles': () => true,
   },
   env: {
     browser: true,
-    es2017: true,
+    es6: true,
     node: true,
   },
 };
